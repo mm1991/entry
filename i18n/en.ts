92d0da25c2b6210c43a334a6e123f63d3d97a421
Lidan Mei <lidan.mei@shopee.com>
2021-07-08 13:55:56 +0800

    clear

--- i18n/en.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'BLACK CAT',
-  slogan: 'FIND THE MOST LOVED ACTIVITIES',
-  signin: 'SIGN IN',
-  usernamePlaceholder: 'Username',
-  passwordPlaceholder: 'Pssword',
-  switch: 'Switch Language',
-};
